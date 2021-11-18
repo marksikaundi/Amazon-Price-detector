e74ac139479355a922afe43d1a0feede08db8ba8
Mark Sikaundi <sikaundimark@gmail.com>
2021-11-18 02:43:22 +0200

    all files clean and runing

---
+++ amazon_config.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+
+DIRECTORY = 'reports'
+NAME = 'PS4'
+CURRENCY = '€'
+MIN_PRICE = '275'
+MAX_PRICE = '650'
+FILTERS = {
+    'min': MIN_PRICE,
+    'max': MAX_PRICE
+}
+BASE_URL = "http://www.amazon.de/"
+
+
+def get_chrome_web_driver(options):
+    return webdriver.Chrome("./chromedriver", chrome_options=options)
+
+
+def get_web_driver_options():
+    return webdriver.ChromeOptions()
+
+
+def set_ignore_certificate_error(options):
+    options.add_argument('--ignore-certificate-errors')
+
+
+def set_browser_as_incognito(options):
+    options.add_argument('--incognito')
+
+
+def set_automation_as_head_less(options):
+    options.add_argument('--headless')
